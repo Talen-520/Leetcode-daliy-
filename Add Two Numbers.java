725c2fdfc4e342f8d9c04d32cee5cc7dd030933f
Talen-520 <63370853+Talen-520@users.noreply.github.com>
2021-12-29 22:37:40 -0500

    Update Add Two Numbers.java

--- Add Two Numbers.java
+++ Add Two Numbers.java
@@ -36,4 +36,26 @@ class Solution {
         return r;
     }    
 }
+ 
 
+//solution 2
+//https://leetcode.com/problems/add-two-numbers/discuss/1044/Java-concise-solution.
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    int carry = 0;
+    ListNode p, dummy = new ListNode(0);
+    p = dummy;
+    while (l1 != null || l2 != null || carry != 0) {
+        if (l1 != null) {
+            carry += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            carry += l2.val;
+            l2 = l2.next;
+        }
+        p.next = new ListNode(carry%10);
+        carry /= 10;
+        p = p.next;
+    }
+    return dummy.next;
+}
